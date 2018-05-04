a8d7ca4f70f5b9b733ac1ce5bbee7658a47813a5
Alberto Jimenez de los Galanes Miralles <a.jimenezmir@alumnos.urjc.es>
2018-05-04 17:02:03 +0200

    Repair fusion v2

--- src/routes/unit/unit.route.ts
+++ src/routes/unit/unit.route.ts
@@ -14,7 +14,7 @@ unitRoutes.get("/search/:name", (req, res) => {
 unitRoutes.get("", (req, res) => {
     unitController.findAll(req, res);
 });
-homeRoutes.delete("/:id", (req, res) => {
+unitRoutes.delete("/:id", (req, res) => {
     unitController.delete(req, res);
 });
 
