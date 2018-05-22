52d57ffe1a61664b9ac840b9ba9d9c563b764d87
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-05-22 13:18:43 +0200

    Fixing conflicts

--- src/controllers/unit.controller.ts
+++ src/controllers/unit.controller.ts
@@ -16,52 +16,49 @@ export class UnitController {
         this.unitResource = new UnitResource();
         this.relationResource = new RelationResource();
     }
-
-  async getFriendsByUnit(req: Request, res: Response) {
-    const unit: Unit = await this.unitResource.findByCode(req.params.id);
-    const topUnitsId: number[] = await this.relationResource.findIdByLowerUnit(unit.getId());
-    const topUnits: Unit[] = await this.unitResource.getUnits(topUnitsId);
-    const lowerUnitsId: number[] = Array.from(await this.unitResource.getFriends(unit.getId(), 5));
-    const lowerUnits: Unit[] = await this.unitResource.getUnits(lowerUnitsId);
-    const relations: Relation[] = await this.relationResource.getRelations(topUnits.concat(unit).concat(lowerUnits));
-    const cincoNivelesOutput: CincoNivelesOutputDto = { unit: unit, topUnits: topUnits, lowerUnits: lowerUnits, relations: relations };
-    cincoNivelesOutput ? res.status(HttpStatusCode.OK).json(cincoNivelesOutput) : res.status(HttpStatusCode.NOT_FOUND).end();
-  }
-
-  async create(req: Request, res: Response): Promise<any> {
-    const unit: Unit = await this.unitResource.create(req.body.name);
-    unit ? res.status(HttpStatusCode.CREATED).json(unit) : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
-}
-async findByName(req: Request, res: Response) {
-    const name: string = req.query.name;
-    logger.info(name);
-    const unitOutputDtos: UnitOutputDto[] = await this.unitResource.findByName(name);
-    unitOutputDtos ? res.status(HttpStatusCode.OK).json(unitOutputDtos) : res.status(HttpStatusCode.NOT_FOUND).end();
-}
-async findAll(req: Request, res: Response): Promise<any> {
-    const units: Unit[] = await this.unitResource.findAll();
-    units ? res.status(HttpStatusCode.OK).json(units) : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
-}
-async delete(req: Request, res: Response): Promise<any> {
-    const code: number = req.params.code;
-    const unit: Unit = await this.unitResource.findByCode(code);
-    if (unit) {
-        const success: boolean = await this.unitResource.delete(code);
-        success ? res.status(HttpStatusCode.NO_CONTENT).end() : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
-    } else {
-        res.status(HttpStatusCode.NOT_FOUND).end();
+    
+    async getFriendsByUnit(req: Request, res: Response) {
+        const unit: Unit = await this.unitResource.findByCode(req.params.id);
+        const topUnitsId: number[] = await this.relationResource.findIdByLowerUnit(unit.getId());
+        const topUnits: Unit[] = await this.unitResource.getUnits(topUnitsId);
+        const lowerUnitsId: number[] = Array.from(await this.unitResource.getFriends(unit.getId(), 5));
+        const lowerUnits: Unit[] = await this.unitResource.getUnits(lowerUnitsId);
+        const relations: Relation[] = await this.relationResource.getRelations(topUnits.concat(unit).concat(lowerUnits));
+        const cincoNivelesOutput: CincoNivelesOutputDto = { unit: unit, topUnits: topUnits, lowerUnits: lowerUnits, relations: relations };
+        cincoNivelesOutput ? res.status(HttpStatusCode.OK).json(cincoNivelesOutput) : res.status(HttpStatusCode.NOT_FOUND).end();
+    }
+    async create(req: Request, res: Response): Promise<any> {
+        const unit: Unit = await this.unitResource.create(req.body.name);
+        unit ? res.status(HttpStatusCode.CREATED).json(unit) : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
+    }
+    async findByName(req: Request, res: Response) {
+        const name: string = req.query.name;
+        logger.info(name);
+        const unitOutputDtos: UnitOutputDto[] = await this.unitResource.findByName(name);
+        unitOutputDtos ? res.status(HttpStatusCode.OK).json(unitOutputDtos) : res.status(HttpStatusCode.NOT_FOUND).end();
+    }
+    async findAll(req: Request, res: Response): Promise<any> {
+        const units: Unit[] = await this.unitResource.findAll();
+        units ? res.status(HttpStatusCode.OK).json(units) : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
+    }
+    async delete(req: Request, res: Response): Promise<any> {
+        const code: number = req.params.code;
+        const unit: Unit = await this.unitResource.findByCode(code);
+        if (unit) {
+            const success: boolean = await this.unitResource.delete(code);
+            success ? res.status(HttpStatusCode.NO_CONTENT).end() : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
+        } else {
+            res.status(HttpStatusCode.NOT_FOUND).end();
+        }
+    }
+    async findById(req: Request, res: Response): Promise<any> {
+        const id: number = req.params.id;
+        const unit: Unit = await this.unitResource.findById(id);
+        unit ? res.status(HttpStatusCode.OK).json(unit) : res.status(HttpStatusCode.NOT_FOUND).end();
+    }
+    async findByCode(req: Request, res: Response): Promise<any> {
+        const code: number = req.params.code;
+        const unit: Unit = await this.unitResource.findByCode(code);
+        unit ? res.status(HttpStatusCode.OK).json(unit) : res.status(HttpStatusCode.NOT_FOUND).end();
     }
 }
-async findById(req: Request, res: Response): Promise<any> {
-    const id: number = req.params.id;
-    const unit: Unit = await this.unitResource.findById(id);
-    unit ? res.status(HttpStatusCode.OK).json(unit) : res.status(HttpStatusCode.NOT_FOUND).end();
-}
-async findByCode(req: Request, res: Response): Promise<any> {
-    const code: number = req.params.code;
-    const unit: Unit = await this.unitResource.findByCode(code);
-    unit ? res.status(HttpStatusCode.OK).json(unit) : res.status(HttpStatusCode.NOT_FOUND).end();
-}
-
-}
-
