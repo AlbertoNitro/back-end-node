04e4e3f3548fe19619f7f0caf3ae79d56ef126fb
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-05-23 12:09:08 +0200

    Mejora de tests

--- test/controllers/relation.controller.test.ts
+++ test/controllers/relation.controller.test.ts
@@ -16,7 +16,7 @@ const dbService: DbService = new DbService();
 
 describe("POST /relation", () => {
     it("should return: 201 - CREATED + Relation", (done) => {
-        const relationInputDto: RelationInputDto = {type: TypeRelation.COMPOSE, topUnitCode: 60, lowerUnitCode: 61};
+        const relationInputDto: RelationInputDto = {type: TypeRelation.COMPOSE, topUnitCode: 51, lowerUnitCode: 52};
         return request(app).post("/relation")
             .send(relationInputDto)
             .end( (err, res) => {
