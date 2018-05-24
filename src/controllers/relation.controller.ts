dd5a699e94836341fd4e947b358050f1857ae3a5
Chinegua <ai.chinegua@gmail.com>
2018-05-24 13:12:42 +0200

    solve post relation

--- src/controllers/relation.controller.ts
+++ src/controllers/relation.controller.ts
@@ -19,8 +19,9 @@ export class RelationController {
         relations ? res.status(HttpStatusCode.OK).json(relationOutputDtos) : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
     }
     async create(req: Request, res: Response): Promise<any> {
-        const relationDto: RelationInputDto = req.body;
-        const relation: Relation = await this.relationResource.create(relationDto);
+
+        const relationInputDto: RelationInputDto = req.body;
+        const relation: Relation = await this.relationResource.create(relationInputDto);
         const relationOutputDto: RelationOutputDto = RelationController.toRelationOutputDto(relation);
         relation ? res.status(HttpStatusCode.CREATED).json(relationOutputDto) : res.status(HttpStatusCode.BAD_REQUEST).end();
     }
