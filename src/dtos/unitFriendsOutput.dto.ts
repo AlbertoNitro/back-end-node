d7b3ed21fe10e8177ba35f974aee1d574c570ee6
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-05-24 13:28:35 +0200

    Fixing errors

--- src/dtos/unitFriendsOutput.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Unit } from "../models/unit.model";
-import { Relation } from "../models/relation.model";
-
-export interface UnitFriendsOutputDto {
-    unit: Unit;
-    topUnits: Unit[];
-    lowerUnits: Unit[];
-    relations: Relation;
-}
