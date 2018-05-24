0f89a0e6845459e85396cbbc69cc8a09a30b2a94
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-05-24 09:34:44 +0200

    dto relacion salida

--- src/controllers/relation.controller.ts
+++ src/controllers/relation.controller.ts
@@ -15,16 +15,16 @@ export class RelationController {
 
     async findAll(req: Request, res: Response): Promise<any> {
         const relations: Relation[] = await this.relationResource.findAll();
-        const relationOutputDtos: RelationOutputDto[] = this.toArrayRelationOutputDto(relations);
+        const relationOutputDtos: RelationOutputDto[] = RelationController.toArrayRelationOutputDto(relations);
         relations ? res.status(HttpStatusCode.OK).json(relationOutputDtos) : res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).end();
     }
     async create(req: Request, res: Response): Promise<any> {
         const relationDto: RelationInputDto = req.body;
         const relation: Relation = await this.relationResource.create(relationDto);
-        const relationOutputDto: RelationOutputDto = this.toRelationOutputDto(relation);
+        const relationOutputDto: RelationOutputDto = RelationController.toRelationOutputDto(relation);
         relation ? res.status(HttpStatusCode.CREATED).json(relationOutputDto) : res.status(HttpStatusCode.BAD_REQUEST).end();
     }
-    private toRelationOutputDto(relation: Relation): RelationOutputDto {
+    private static toRelationOutputDto(relation: Relation): RelationOutputDto {
         const relationOutputDto: RelationOutputDto = {
             type: relation.getType(),
             topUnit: {name: relation.getTopUnit().getName(), code: relation.getTopUnit().getCode()},
@@ -35,10 +35,10 @@ export class RelationController {
         };
         return relationOutputDto;
     }
-    private toArrayRelationOutputDto(relations: Relation[]): RelationOutputDto[] {
+    private static toArrayRelationOutputDto(relations: Relation[]): RelationOutputDto[] {
         const relationOutputDtos: RelationOutputDto[] = [];
         for (let i = 0 ; i < relations.length ; i++ ) {
-            relationOutputDtos.push(this.toRelationOutputDto(relations[i]));
+            relationOutputDtos.push(RelationController.toRelationOutputDto(relations[i]));
         }
         return relationOutputDtos;
     }
