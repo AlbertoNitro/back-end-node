d44b0b1186c8cbd74a60fb005776ee3f626d971c
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-06-01 13:41:18 +0200

    dfsdfsd

--- src/services/daos/interaction.dao.ts
+++ src/services/daos/interaction.dao.ts
@@ -7,10 +7,29 @@ import { ExerciseDao } from "./exercise.dao";
 import { Video } from "../../models/video.model";
 import { Exercise } from "../../models/exercise.model";
 import { Solution } from "../../models/solution.model";
+import { VideoInteractionInput } from "../../dtos/videoInteractionInput.dto";
+import VideoSchema from "../../schemas/video.schema";
+import { ExerciseInteractionInput } from "../../dtos/exerciseInteractionInput.dto";
+import ExerciseSchema from "../../schemas/exercise.schema";
 
 export class InteractionDao {
     constructor() {
     }
+    async createVideo(videoII: VideoInteractionInput) {
+        console.log("Creando " + JSON.stringify(videoII.kind));
+        VideoSchema.create(videoII)
+            .then((interaction: Document) => {
+                console.log(interaction);
+            });
+    }
+    async createExercise(exerciseII: ExerciseInteractionInput) {
+        console.log("Creando " + JSON.stringify(exerciseII.kind));
+        ExerciseSchema.create(exerciseII)
+            .then((interaction: Document) => {
+                console.log(interaction);
+            });
+    }
+
     public static toInteraction(document: Document): Interaction {
         let interaction: Interaction;
         if (InteractionDao.isVideo(document)) {
