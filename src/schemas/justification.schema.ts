144a29acaaa2e7627f35eeaeb301759e9a4e40b6
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-06-01 13:56:31 +0200

    COMPILA

--- src/schemas/justification.schema.ts
+++ src/schemas/justification.schema.ts
@@ -1,15 +1,9 @@
 import mongoose from "mongoose";
 
 const Justification = new mongoose.Schema({
-    _id: Number,
     isCorrect: { type: Boolean, required: true },
     text: { type: String, required: true },
-<<<<<<< HEAD
-}, { _id: false });
-Justification.plugin(AutoIncrement);
-=======
 });
->>>>>>> develop
 
 
 const JustificationSchema = mongoose.model("Justification", Justification);
