64418b504d3038040f6f2d5d1adccc2202f30be8
Chinegua <ai.chinegua@gmail.com>
2018-06-04 10:01:03 +0200

    refactor

--- src/schemas/formation.schema.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const Formation = new mongoose.Schema({
-    name: { type: String, required: true },
-    unit: { type: mongoose.Schema.Types.ObjectId, ref: "Unit", required: true },
-});
-
-const FormationSchema = mongoose.model("Formation", Formation);
-export default FormationSchema;
--- src/schemas/lessonsItinerary.schema.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const LessonsItinerary = new mongoose.Schema({
-    name: { type: String, required: true },
-    session: { type: mongoose.Schema.Types.ObjectId, ref: "Session", required: true },
-});
-
-const LessonsItinerarySchema = mongoose.model("Lesson", LessonsItinerary);
-export default LessonsItinerarySchema;
