3e0e308ee38e06b773141f64231ddb80242fdc3f
Alberto Jimenez de los Galanes Miralles <a.jimenezmir@alumnos.urjc.es>
2018-06-04 15:05:00 +0200

    Solving conflicts

--- src/services/daos/interaction.dao.ts
+++ src/services/daos/interaction.dao.ts
@@ -1,31 +1,21 @@
-import { Document } from "mongoose";
-import { Video } from "../../models/video.model";
-import VideoSchema from "../../schemas/video.schema";
-<<<<<<< HEAD
+import { Unit } from "../../models/unit.model";
 import logger from "../../util/logger";
-=======
+import UnitSchema from "../../schemas/unit.schema";
+import { UnitBuilder } from "../../models/builders/unit.builder";
+import { Document } from "mongoose";
+import { VideoInteractionInput } from "../../dtos/videoInteractionInput.dto";
+import InteractionSchema from "../../schemas/interaction.schema";
+import VideoSchema from "../../schemas/video.schema";
 import { ExerciseInteractionInput } from "../../dtos/exerciseInteractionInput.dto";
 import ExerciseSchema from "../../schemas/exercise.schema";
+import { Interaction } from "../../models/interaction.model";
 import { VideoBuilder } from "../../models/builders/video.builder";
 import { ExerciseBuilder } from "../../models/builders/exercise.builder";
->>>>>>> develop
 
 export class InteractionDao {
 
     constructor() {
     }
-<<<<<<< HEAD
-    // public static toInteraction(document: Document): Video {
-    //     return new Video(document.get("url")).setId(document.get("_id"));
-    // }
-    // public static toArrayInteractions(documents: Document[]): Video[] {
-    //     const videos: Video[] = [];
-    //     for (let i = 0; i < documents.length; i++) {
-    //         videos.push(VideoDao.toVideo(documents[i]));
-    //     }
-    //     return videos;
-    // }
-=======
     private static toInteraction(document: Document): Interaction {
         if ( document.get("kind") == "Video") {
             return new VideoBuilder(document.get("url")).setId(document.get("_id")).build();
@@ -42,19 +32,18 @@ export class InteractionDao {
         console.log("interaction " + interaction);
         return interaction;
     }
-
     async createVideo(videoII: VideoInteractionInput) {
         console.log("Creando " + JSON.stringify(videoII.kind));
         VideoSchema.create(videoII)
-            .then((interaction: Document) => {
-                console.log(interaction);
-            });
+        .then((interaction: Document) => {
+            console.log(interaction);
+        });
     }
     async createExercise(exerciseII: ExerciseInteractionInput) {
         ExerciseSchema.create(exerciseII)
-            .then((interaction: Document) => {
-                console.log(interaction);
-            });
+        .then((interaction: Document) => {
+            console.log(interaction);
+        });
     }
 
     async findAll(): Promise<Interaction[]> {
@@ -68,38 +57,4 @@ export class InteractionDao {
                 return undefined;
             });
     }
-
-    public static toArrayInteractions(documents: Document[]): Interaction[] {
-        const interactions: Interaction[] = [];
-        for (let i = 0; i < documents.length; i++) {
-            interactions.push(InteractionDao.toInteraction(documents[i]));
-        }
-        return interactions;
-    }
-    private static isVideo(document: Document): boolean {
-        logger.info(" document.get(\"url\") + " +  document.get("url"));
-        return document.get("url");
-    }
-    async delete(id: number): Promise<boolean> {
-        return await InteractionSchema.deleteOne({_id: id })
-            .then( () => {
-                return true;
-            })
-            .catch ( err => {
-                logger.error(err);
-                return false;
-            });
-    }
-    async findById(id: number): Promise<Interaction> {
-        return await InteractionSchema.findById(id)
-            .then( (interactionDocument: Document) => {
-                const interaction: Interaction = interactionDocument ? InteractionDao.toInteraction(interactionDocument) : undefined;
-                return interaction;
-            })
-            .catch ( err => {
-                logger.error(err);
-                return undefined;
-            });
-    }
->>>>>>> develop
 }
