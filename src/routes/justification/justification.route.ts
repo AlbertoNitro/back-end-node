f7fc33698e59dc299bb386391cc09c0b8376b464
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-06-05 10:33:41 +0200

    Implemented test CREATE justification

--- src/routes/justification/justification.route.ts
+++ src/routes/justification/justification.route.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import { JustificationController } from "../../controllers/justification.controller";
 
 const justificationRoutes = express.Router();
+const justificationController: JustificationController = new JustificationController();
 
-justificationRoutes.get("", (req, res) => {
+justificationRoutes.post("", (req, res) => {
+    justificationController.create(req, res);
+});
+justificationRoutes.get("/:id", (req, res) => {
+    justificationController.findById(req, res);
+});
+justificationRoutes.delete("/:id", (req, res) => {
+    justificationController.delete(req, res);
 });
 
 export default justificationRoutes;
