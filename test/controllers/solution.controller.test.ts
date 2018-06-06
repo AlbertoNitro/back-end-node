c24155c938149126afe94b5fbc214585c7c1f3cd
Chinegua <ai.chinegua@gmail.com>
2018-06-06 11:24:42 +0200

    adding solution test

--- test/controllers/solution.controller.test.ts
+++ test/controllers/solution.controller.test.ts
@@ -12,7 +12,17 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const dbService: DbService = new DbService();
-
+beforeAll( async (done) => {
+    const successDeleteDb: boolean = await dbService.delete();
+    if (!successDeleteDb) {
+        logger.error("Abortando lanzamiento de pruebas, fallo al resetear DB.");
+    }
+    const successSeedDb: boolean = await dbService.seed();
+    if (!successSeedDb) {
+        logger.error("Abortando lanzamiento de pruebas, fallo al poblar DB.");
+    }
+    successSeedDb && successDeleteDb ? done() : fail("Abortando lanzamiento de pruebas...");
+});
 describe("POST /solution", () => {
     it("should return: 201 - CREATED + Solution", (done) => {
         return request(app).post("/solution")
@@ -24,6 +34,38 @@ describe("POST /solution", () => {
                 expect(res.status).to.equal(HttpStatusCode.CREATED);
                 const solutionOutputDto: SolutionOutputDto = res.body;
                 expect(solutionOutputDto.text).to.equal("Prueba");
+                done();
+            });
+    });
+});
+
+describe("GET /solution", () => {
+    it("should return: 200", (done) => {
+        return request(app).get("/solution")
+            .end(  async (err, res) => {
+                expect(res.status).to.equal(HttpStatusCode.OK);
+                const solutionOutputDto: SolutionOutputDto[] = res.body;
+                expect(solutionOutputDto.length).to.equal(4);
+                done();
+            });
+    });
+});
+describe("GET /solution/:id", () => {
+    it("should return: 200", (done) => {
+        return request(app).get("/solution/561d87b8b230cf35177998cc")
+            .end(  async (err, res) => {
+                expect(res.status).to.equal(HttpStatusCode.OK);
+                const solutionOutputDto: SolutionOutputDto = res.body;
+                expect(solutionOutputDto.text).to.equal("La formula del agua H20");
+                done();
+            });
+    });
+});
+describe("DELETE /solution/:id", () => {
+    it("should return: 204", (done) => {
+        return request(app).delete("/solution/561d87b8b230cf35177998cc")
+            .end(  async (err, res) => {
+                expect(res.status).to.equal(HttpStatusCode.NO_CONTENT);
                 done();
             });
     });
