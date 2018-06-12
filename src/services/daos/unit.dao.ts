171ee9aaccf0a579f68c5534d31aa12c42756537
Chinegua <ai.chinegua@gmail.com>
2018-06-12 15:00:30 +0200

    changes /content

--- src/services/daos/unit.dao.ts
+++ src/services/daos/unit.dao.ts
@@ -68,6 +68,7 @@ export class UnitDao {
         return await UnitSchema.find({})
             .then( (unitsDocument: Document[]) => {
                 const units: Unit[] = unitsDocument ? UnitDao.toArrayUnits(unitsDocument) : undefined;
+                console.log(unitsDocument);
                 return units;
             })
             .catch ( err => {
